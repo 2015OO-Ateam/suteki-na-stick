fb9ef9cac02bf64649ef75bcbfaa98cc894eea74
toshi05 <toshi.n.0520@gmail.com>
2015-07-12 20:26:28 +0900

    add enum column

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 
   before_action :authenticate_user!, only: :show
   def index
+    @users = User.all
   end
 
   def show
--- db/migrate/20150711104632_add_details_to_users.rb
+++ db/migrate/20150711104632_add_details_to_users.rb
@@ -4,6 +4,11 @@ class AddDetailsToUsers < ActiveRecord::Migration
     add_column :users, :last_name, :string
     add_column :users, :title, :string
     add_column :users, :room, :int
-    add_column :users, :status, :integer
+    add_column :users, :status, :int
+    add_index :users, :first_name
+    add_index :users, :last_name
+    add_index :users, :title
+    add_index :users, :room
+    add_index :users, :status
   end
 end
