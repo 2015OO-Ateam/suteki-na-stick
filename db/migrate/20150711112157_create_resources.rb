5f62b4d8f0d5cf21a1782e2ab3a935a60f9ad70c
toshi05 <toshi.n.0520@gmail.com>
2015-07-12 23:07:46 +0900

    merge

--- db/migrate/20150711112157_create_resources.rb
+++ db/migrate/20150711112157_create_resources.rb
@@ -5,8 +5,7 @@ class CreateResources < ActiveRecord::Migration
       t.integer  :return_status, default: 0, null: false, limit:2
       t.integer  :borrow_status, default: 0, null: false, limit:1
       t.integer  :use_permission, default: 0, null: false, limit:1
-
-      t.integer  :necessary_of_scan, default 0, null: false, limit:1
+      t.integer  :necessary_of_scan, default: 0, null: false, limit:1
 
       t.timestamps null: false
     end
