fc24b9d365a2a1444106f78fca1ca8ea341ae993
toshi05 <toshi.n.0520@gmail.com>
2015-07-16 07:32:25 +0900

    add edit lab_pc and software

--- app/controllers/software_controller.rb
+++ app/controllers/software_controller.rb
@@ -3,4 +3,20 @@ class SoftwareController < ApplicationController
   def edit
     @software = Software.find(params[:id])
   end
+  def change
+    @software = Software.find(params[:id])
+  end
+  def change_update
+      @software = Software.find(params[:id])
+      if @software.update(software_params)
+        redirect_to resources_path
+      else
+        render 'change'
+      end
+    end
+
+    private
+      def software_params
+        params[:software].permit(:limit)
+      end
 end
